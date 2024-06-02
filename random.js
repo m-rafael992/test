f0e78a6eeaf33483a592b332dbc4f55c89c672bd
Rafael <rafa92muhitdinov@gmail.com>
2024-06-03 00:54:18 +0500

    save2

--- random.js
+++ random.js
@@ -1,13 +1,4 @@
 module.exports = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    
-    if (req.method === 'OPTIONS') {
-      res.status(204).end();
-      return;
-    }
-    
     const randomNumber = Math.floor(Math.random() * 100); // Генерируем случайное число от 0 до 99
     res.status(200).json({ number: randomNumber });
   };
